e12bfe02c6155bff607e455f4f4fbc766bb60ac3
Joe Esposito <joe@joeyespo.com>
2014-09-26 13:16:24 -0400

    Expose gesso.canvas to the client.

--- gesso/index.js
+++ gesso/index.js
@@ -1,7 +1,17 @@
-var command = require('./command');
+// Re-using Gesso entry point
+// Detect whether this is called from a built bundle from the browser, or as the build project.
 
 
-module.exports = {
-  globalMain: command.globalMain,
-  main: command.main
-};
+if (typeof window !== 'undefined') {
+  // Client-side require
+  module.exports = {
+    canvas: document.getElementById('gesso-target')
+  };
+} else {
+  // Server-side require -- use module.require so the build doesn't detect this
+  var command = module.require('./command');
+  module.exports = {
+    globalMain: command.globalMain,
+    main: command.main
+  };
+}
