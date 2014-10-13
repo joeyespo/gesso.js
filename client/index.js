2f7dcc7770ceeffc419c0650bbb9ae1e9b88cf62
Joe Esposito <joe@joeyespo.com>
2014-10-12 20:26:00 -0400

    Switch to getCanvas to load canvases lazily.

--- client/index.js
+++ client/index.js
@@ -1,3 +1,22 @@
+function getCanvas() {
+  // TODO: Read the project settings use the right ID
+  var canvas = window.document.getElementById('gesso-target');
+
+  if (!canvas) {
+    var canvases = window.document.getElementsByTagName('canvas');
+    if (canvases.length === 1) {
+      canvas = canvases[0];
+    }
+  }
+
+  if (!canvas) {
+    throw new Error('Canvas not found.');
+  }
+
+  return canvas;
+}
+
+
 module.exports = {
-  canvas: window.document.getElementById('gesso-target')
+  getCanvas: getCanvas
 };
