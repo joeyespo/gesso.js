9ea72a0a10b980d31ea3ee2a4996e0bc84341844
Joe Esposito <joe@joeyespo.com>
2015-02-21 12:10:52 -0500

    Allow inspection of Delegate's handlers and clean up.

--- client/delegate.js
+++ client/delegate.js
@@ -4,25 +4,28 @@ var util = require('./util');
 // Returns a callable object that, when called with a function, subscribes
 // to the delegate. Call invoke on this object to invoke each handler.
 function Delegate() {
-  var self = this;
-  self.handlers = [];
+  var handlers = [];
+
   function callable(handler) {
     if (arguments.length !== 1) {
       throw new Error('Delegate takes exactly 1 argument (' + arguments.length + ' given)');
     } else if (typeof handler !== 'function') {
       throw new Error('Delegate argument must be a Function object (got ' + typeof handler + ')');
     }
-    self.handlers.push(handler);
+    handlers.push(handler);
     return function unsubscribe() {
-      return util.removeLast(self.handlers, handler);
+      return util.removeLast(handlers, handler);
     };
   }
   callable.invoke = function invoke() {
     var args = arguments;
-    util.forEach(self.handlers, function (handler) {
+    util.forEach(handlers, function (handler) {
       handler.apply(null, args);
     });
   };
+  // Expose handlers for inspection
+  callable.handlers = handlers;
+
   return callable;
 }
 
